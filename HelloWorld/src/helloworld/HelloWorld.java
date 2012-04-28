741eb835fe8330df176f63cef1d8268a1e6f4883
jarikos <jari.m.koskinen@helsinki.fi>
2012-04-28 11:33:28 +0300

    modified:   HelloWorld/src/helloworld/HelloWorld.java

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -15,10 +15,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-<<<<<<< HEAD
-       
-=======
->>>>>>> haara
+
         System.out.println("Hello World");
     }
 }
